7aba682083fc21667b51fdf7710b7c01fcb777a3
Milco Numan <milco.numan@gmail.com>
2024-12-17 17:08:59 +0100

    Finished part 8

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const client = require('./elasticsearch/client');
+const data = (require('./data_management/retrieve_and_ingest_data'));
 
 const app = express();
 
 const port = 3001;
+
+app.use('/ingest_data', data)
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
